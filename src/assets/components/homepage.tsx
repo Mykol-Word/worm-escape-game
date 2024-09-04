3b13e185853c1b1f297006ab0a4d4aad2e92e715
Mykol Word <michaelward1916@gmail.com>
2024-09-04 13:40:46 -0700

    Cleaned up CSS and improved menu design

--- src/assets/components/homepage.tsx
+++ src/assets/components/homepage.tsx
@@ -5,16 +5,16 @@ function HomePage() {
 
     return (
       <div className="vh-100 d-flex justify-content-center align-items-center"style={{backgroundImage: `url(${backgroundImage}`}}>
-        <div className="rounded text-center p-5 m-2" style={{backgroundColor: `rgba(0, 0, 0, 0.5)`}}>
+        <div className="menu-container">
           <h1 className="display-1 pb-3 text-white">Worm Escape</h1>
           <Link to="/play" style={{ textDecoration: 'none' }}>
-            <h1 className="display-5 button">Play</h1>
+            <h1 className="button">Play</h1>
           </Link>
           <Link to="/settings" style={{ textDecoration: 'none' }}>
-            <h1 className="display-5 button">Settings</h1>
+            <h1 className="button">Settings</h1>
           </Link>
           <Link to="/about" style={{ textDecoration: 'none' }}>
-            <h1 className="display-5 button">About</h1>
+            <h1 className="button">About</h1>
           </Link>
         </div>
       </div>
