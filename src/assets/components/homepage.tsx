0b966a18beeeb2385e79101ce153a5bb628cf58a
Mykol Word <michaelward1916@gmail.com>
2024-09-04 13:10:38 -0700

    Replaced navbar with title menu and set up routing
    - Removed navbar component
    
    - Made routing title menu with stylized hovering
    
    - Set up rudimentary react routing system

--- src/assets/components/homepage.tsx
+++ src/assets/components/homepage.tsx
@@ -1,19 +1,22 @@
+import { Link } from "react-router-dom";
 import backgroundImage from "../images/homepagebackgroundblurred.jpg";
 
 function HomePage() {
 
     return (
-      <div 
-        className="vh-100 d-flex justify-content-center align-items-center"
-        style={{backgroundImage: `url(${backgroundImage}`}}
-      >
-        <div 
-          className="rounded text-white p-3 m-2" 
-          style={{backgroundColor: `rgba(0, 0, 0, 0.5)`}}
-        >
-          <h1 className="display-1">Worm Escape</h1>
+      <div className="vh-100 d-flex justify-content-center align-items-center"style={{backgroundImage: `url(${backgroundImage}`}}>
+        <div className="rounded text-center p-5 m-2" style={{backgroundColor: `rgba(0, 0, 0, 0.5)`}}>
+          <h1 className="display-1 pb-3 text-white">Worm Escape</h1>
+          <Link to="/play" style={{ textDecoration: 'none' }}>
+            <h1 className="display-5 button">Play</h1>
+          </Link>
+          <Link to="/settings" style={{ textDecoration: 'none' }}>
+            <h1 className="display-5 button">Settings</h1>
+          </Link>
+          <Link to="/about" style={{ textDecoration: 'none' }}>
+            <h1 className="display-5 button">About</h1>
+          </Link>
         </div>
-
       </div>
     )
   }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import HomePage from "../assets/components/homepage";
+
+function Home() {
+
+  return (
+    <>
+      <HomePage></HomePage>
+    </>
+  )
+}
+
+export default Home
