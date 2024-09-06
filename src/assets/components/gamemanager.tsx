0a5ce3557469f11ef880cdd958c67e8450509f76
Mykol Word <michaelward1916@gmail.com>
2024-09-06 13:02:51 -0700

    Escape game is functional but requires focus
    While I added escape game functionality, the  gamemanager element requires focus to play which I don't like. I may figure out how to solve this in the next patch or save this for later.

--- src/assets/components/gamemanager.tsx
+++ src/assets/components/gamemanager.tsx
@@ -8,7 +8,7 @@ import image_jump from "../images/directions/direction_jump.png";
 
 const image_list = [image_up, image_down, image_left, image_right, image_jump];
 
-function generateValidDirectionSequence() {
+const generateValidDirectionSequence = () => {
     let img1 = Math.floor(Math.random() * 5);
     let img2 = Math.floor(Math.random() * 5);
     let img3 = Math.floor(Math.random() * 5);
@@ -18,24 +18,50 @@ function generateValidDirectionSequence() {
 }
 
 //Experimental
-function generateNewDirection(currentSequence: string[]) {
+const generateNewDirection = (currentSequence: string[]) => {
     let newImg = image_list[Math.floor(Math.random() * 5)];
     while(newImg === currentSequence[1])
         newImg = image_list[Math.floor(Math.random() * 5)];
     return newImg
 }
 
+const getCurrentKey = (currentDirection: string) => {
+    if(currentDirection == image_down) { return 's' }
+    else if (currentDirection == image_up) { return 'w' }
+    else if (currentDirection == image_left) { return 'a' }
+    else if (currentDirection == image_right) { return 'd' }
+    else if (currentDirection == image_jump) { return ' ' }
+    else return ''
+}
+
 function GameManger() {
 
-const [directionSequence, setDirectionSequence] = useState(generateValidDirectionSequence());
+    const [directionSequence, setDirectionSequence] = useState(generateValidDirectionSequence());
+    const [seed, setSeed] = useState(Math.random);
 
-return(
-    <div>
-        <img className="direction-image" src={directionSequence[0]}/>
-        <img className="direction-image" src={directionSequence[1]}/>
-        <img className="direction-image" src={directionSequence[2]}/>
-    </div>
-);
+    const forceReload = () => {
+        setSeed(Math.random());
+    }
+
+    const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        const currentKey = getCurrentKey(directionSequence[0]);
+        if(e.key === currentKey) {
+            let tempDirectionSequence = directionSequence;
+            tempDirectionSequence[0] = tempDirectionSequence[1];
+            tempDirectionSequence[1] = tempDirectionSequence[2];
+            tempDirectionSequence[2] = generateNewDirection(directionSequence);
+            setDirectionSequence(tempDirectionSequence);
+        }
+        forceReload();
+    }
+
+    return(
+        <div onKeyDown={handleKeyPress} tabIndex={0}>
+            <img className="direction-image" src={directionSequence[0]}/>
+            <img className="direction-image" src={directionSequence[1]}/>
+            <img key={seed} className="direction-image" src={directionSequence[2]}/>
+        </div>
+    );
 }
 
 export default GameManger;
