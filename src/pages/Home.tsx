3e6a0709d90edfa28f72c10cdd732681739ed9bd
Mykol Word <michaelward1916@gmail.com>
2024-09-06 11:08:59 -0700

    Removed redundant homepage.tsx component
    I've decided to not make the components in the "pages" folder barebones because I think it's useless and redundant.
    
    Instead I am going to make the components in the "pages" folder contain
    all the smaller components and operate the pages main logic.
    
    Since the home page is very minimally functional, this will be better
    reflected in the play page I'm going to start working on.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,28 @@
-import HomePage from "../assets/components/homepage";
+import { Link } from "react-router-dom";
+import backgroundImage from "../assets/images/homepagebackgroundblurred.jpg";
+import titleImage from "../assets/images/homepagetitle.png";
 
 function Home() {
 
   return (
-    <>
-      <HomePage></HomePage>
-    </>
+    <div className="vh-100 d-flex justify-content-center align-items-center"style={{backgroundImage: `url(${backgroundImage}`}}>
+    <div className="menu-container">
+      <a href={"https://store.steampowered.com/app/2318480/Last_Train_Outta_Wormtown/"} ><img className="title-image" src={titleImage}/></a>
+      <div className="text-box">
+        A quick-time escape minigame based off Get(Color) Games' "Last Train Outta' Wormtown."
+        Clicking the title above will take you to the game's store page. 
+      </div>
+      <Link to="/play" style={{ textDecoration: 'none' }}>
+        <h1 className="button">Play</h1>
+      </Link>
+      <Link to="/settings" style={{ textDecoration: 'none' }}>
+        <h1 className="button">Settings</h1>
+      </Link>
+      <Link to="/about" style={{ textDecoration: 'none' }}>
+        <h1 className="button">About</h1>
+      </Link>
+    </div>
+  </div>
   )
 }
 
